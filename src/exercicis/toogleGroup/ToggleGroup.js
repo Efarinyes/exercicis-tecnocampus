58753650d516a3e1aec30edb1f04d71beaafd94d
Efarinyes <efarinyes@gmail.com>
2022-03-23 19:50:58 +0100

    Commit - Tocs finals

--- src/exercicis/toogleGroup/ToggleGroup.js
+++ src/exercicis/toogleGroup/ToggleGroup.js
@@ -1,15 +1,15 @@
 import "./toggleGroup.css";
 import { useState } from "react";
 
-let botoClicat = "";
+// let botoClicat = "";
 function ToggleGroup() {
   const textos = ["Boto-1", "Boto-2", "Boto-3"];
   const [textBoto, setTextBoto] = useState(textos[0]);
+  const [botoClicat, setBotoClicat] = useState("");
 
   const changeTextColor = (text) => {
     setTextBoto(text);
-    botoClicat = text;
-    console.log(botoClicat);
+    botoClicat ? setBotoClicat(null) : setBotoClicat(text);
   };
 
   return (
@@ -18,7 +18,7 @@ function ToggleGroup() {
       <hr />
       {textos.map((text, index) => (
         <button
-          className={text === botoClicat ? "red" : "toggleButton"}
+          className={text === botoClicat ? "red" : "toggleGroupButton"}
           onClick={() => changeTextColor(text)}
           key={index}
         >
